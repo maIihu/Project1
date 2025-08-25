d921bda75b6a4f47205c43a3876a846a47aca851
Nguyễn Mạnh Hùng <nguyenmanhhung20184@gmail.com>
2025-08-25 23:16:09 +0700

    add UI pause panel

---
+++ Assets/__MyGame/Code/Script/UI/UIPausePanel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace __MyGame.Code.Script.UI
+{
+    public class UIPausePanel : MonoBehaviour
+    {
+        private bool _isPaused;
+
+        private void Start()
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        public void ControlPausePanel()
+        {
+            _isPaused = !_isPaused;
+            this.gameObject.SetActive(_isPaused);
+        }
+    }
+}
