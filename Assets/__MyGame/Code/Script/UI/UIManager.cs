efec6bae0fb0910521031d243493051133c81976
Nguyễn Mạnh Hùng <nguyenmanhhung20184@gmail.com>
2025-08-25 23:14:58 +0700

    add post-process

---
+++ Assets/__MyGame/Code/Script/UI/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace __MyGame.Code.Script.UI
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField] private Button pauseButton;
+        [SerializeField] private UIPausePanel pausePanel;
+
+        private void Awake()
+        {
+            pauseButton.onClick.AddListener(OnClickPauseButton);
+        }
+
+        private void OnClickPauseButton()
+        {
+            pausePanel.ControlPausePanel();
+        }
+    }
+}
