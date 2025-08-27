4d7374bdbb6a4a538a7f073ab5fc06eafd838c62
Nguyễn Mạnh Hùng <nguyenmanhhung20184@gmail.com>
2025-08-27 16:12:32 +0700

    create dropdown windowscale

---
+++ Assets/__MyGame/Code/Script/UI/UIWindowScaleOption.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace __MyGame.Code.Script.UI
+{
+    public class UIWindowScaleOption : MonoBehaviour
+    {
+        [SerializeField] private TMP_Dropdown windowScaleDropdown;
+        [SerializeField] private TextMeshProUGUI value;
+
+        private void Awake()
+        {
+            windowScaleDropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+        }
+        
+        private void OnDropdownValueChanged(int index)
+        {
+            string selectedOption = windowScaleDropdown.options[index].text;
+            value.text = selectedOption;
+            Debug.Log("Selected: " + selectedOption);
+            Debug.Log("Current Screen: " + Screen.width + "x" + Screen.height);
+        }
+    }
+}
