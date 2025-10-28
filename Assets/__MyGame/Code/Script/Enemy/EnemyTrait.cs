c0692708ed90d2f026f971689078723c9387b255
ghenkazuchi <ghenkazuchi@gmail.com>
2025-10-28 20:04:07 +0700

    Enemy Stuff

---
+++ Assets/__MyGame/Code/Script/Enemy/EnemyTrait.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyTrait", menuName = "Enemy/Trait")]
+public class EnemyTrait : ScriptableObject
+{
+}
---
+++ Assets/__MyGame/Code/Script/Enemy/TraitInterface/IOnAfterMove.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IOnAfterMove 
+{
+	public void OnAfterMove();
+}
