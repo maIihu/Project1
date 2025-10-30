0d52941b807600e0663b7ddd7f89bfc2ab8af639
Nguyễn Mạnh Hùng <nguyenmanhhung20184@gmail.com>
2025-10-30 11:21:34 +0700

    create: ObjectPool

---
+++ Assets/__MyGame/Code/Script/GameObjectPool.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using uPools;
+
+namespace __MyGame.Code.Script
+{
+    public class GameObjectPool : MonoBehaviour
+    {
+        [SerializeField] private EnemyEntity enemyPrefab;
+        
+        public void InitEnemyPooling()
+        {
+            SharedGameObjectPool.Prewarm(enemyPrefab.gameObject, 1);
+        }
+
+        public EnemyEntity GetEnemy(Vector3 position, Quaternion rotation, Transform parent)
+        {
+            SharedGameObjectPool.Rent(enemyPrefab.gameObject, position, rotation, parent).TryGetComponent(out EnemyEntity enemy);
+            return enemy;
+        }
+    }
+}
